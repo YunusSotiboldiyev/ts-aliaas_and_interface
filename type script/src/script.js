5a5e8d95ed7fda562ab05267a45314832f872eaa
Yunus <152853532+YunusSotiboldiyev@users.noreply.github.com>
2024-10-14 13:04:07 +0500

    script.js

--- type script/src/script.js
+++ type script/src/script.js
@@ -1,13 +1,34 @@
-var employee1 = {
+var student = {
+    name: 'Yunus',
+    age: 16,
+    grade: 3
+};
+var teacher = {
+    name: 'Ms. Dilmurod',
+    age: 35,
+    subject: 'Programme'
+};
+console.log('Student:', student);
+console.log('Teacher:', teacher);
+var account1 = {
     id: 1,
-    name: "Maqsad",
-    position: "Working",
-    salary: 100
+    status: "active"
 };
-var employee2 = {
-    id: 2,
-    name: "Samandar",
-    position: "studing",
-    salary: 200
+var account2 = {
+    id: "A123",
+    status: "pending"
 };
-console.log(employee1, employee2);
+console.log('Account 1:', account1);
+console.log('Account 2:', account2);
+function calculateDiscountedPrice(product, discountPercentage) {
+    var discount = (product.price * discountPercentage) / 100;
+    return product.price - discount;
+}
+var product = {
+    name: 'Laptop',
+    price: 1000,
+    inStock: true
+};
+var discountedPrice = calculateDiscountedPrice(product, 10);
+console.log("Original Price: $".concat(product.price));
+console.log("Discounted Price: $".concat(discountedPrice));
